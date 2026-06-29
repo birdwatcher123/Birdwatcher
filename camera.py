6cbf8169299f1c67508cdd9e74eeda59b92951b4
Sude Ikbal Koc <sude_ikbal.koc@hsbi.de>
2026-06-29 21:44:59 +0200

    camera.py

--- camera.py
+++ camera.py
@@ -1,19 +1,25 @@
-from picamera import PiCamera #Import des RaspberryPi-Camera Bibliothek
-import logging #Import der Bibliothek zum Erstellen eines Logfiles
-from time import sleep #Import der Time-Bibliothek für die Funktion sleep()
+from picamera2 import Picamera2  
+import logging
+from time import sleep
 
 class camera():
-    
-    def takePhoto(pFile): #Definition der Methode takePhoto mit dem Paramter pFile (Speicherort des aufzunehmenden Fotos)
-    
+
+    def takePhoto(pFile):  # pFile = Speicherort des Fotos
+
         try:
-            camera = PiCamera() 
-            camera.rotation =180 #Drehen des Kamerabildes um 180°
-            camera.start_preview() #Start der Kamera
-            sleep(2) #Warten (2 Sekunden), für die Lichtverhältnisse
-            camera.capture(pFile) #Aufnahme des Fotos an den vorgegebenen Speicherort
-            camera.stop_preview() #Stop der Kamera
+            camera = Picamera2()
+
+            camera.video_configuration.transform.vflip = True
+            camera.video_configuration.transform.hflip = True
+            
+            config = camera.create_still_configuration()  
+            camera.configure(config)
+            
+            camera.start()        # Kamera starten
+            sleep(2)              
+            camera.capture_file(pFile)  # Foto aufnehmen und speichern
+            camera.stop()         # Kamera stoppen
+            camera.close()        # Kamera freigeben
+            
         except Exception as a:
-            logging.error(a) #Logging, falls Exceptions auftreten
-        
-    
+            logging.error(a)
